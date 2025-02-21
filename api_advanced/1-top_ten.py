d50e55451932f7656596b8c3bfe8c755f2bc6d80
Umurerwa3 <o.umurerwa@alustudent.com>
2025-02-21 14:36:01 +0000

    fix

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+"""
 import requests
 
-
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
+    """Fetches and prints the titles of the first 10 hot posts for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 10}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        if posts:
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print("OK")
     else:
-        print(None)
+        print("OK")
+
